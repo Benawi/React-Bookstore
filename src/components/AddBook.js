2ee11db203e3476a0865ba91e3d3fa9274759025
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-06-16 12:27:43 +0300

    Add book generes for add book section

---
+++ src/components/AddBook.js
@@ -0,0 +1,16 @@
+import { v4 as uuidv4 } from 'uuid';
+
+const bookGenres = [
+  'Fiction',
+  'Non-fiction',
+  'History',
+  'Romance',
+  'Science Fiction/Fantasy',
+  'Mystery/Thriller',
+  'Horror',
+  'Young Adult',
+  'Dystopian',
+  'Realist',
+  'Childrens books',
+  'Poetry',
+];
