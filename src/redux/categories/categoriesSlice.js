91fd0da742885b1dfd628c62f62b052cf9f68a8b
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-06-19 21:31:40 +0300

    Includes a reducer that checks the status

--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -5,3 +5,15 @@ const initialState = {
   status: 'Under construction',
 };
 
+const categoriesSlice = createSlice({
+  name: 'categories',
+  initialState,
+  reducers: {
+    toggleStatus: (state) => {
+      state.status = 'Under construction';
+    },
+  },
+});
+
+export const toggleStatus = categoriesSlice.actions;
+export default categoriesSlice.reducer;
