075b0fc3298abaafa8dfa282e96115a4c1d4233a
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-06-19 21:29:18 +0300

    Add reducer that add a book

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -3,4 +3,12 @@ const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 
 const initialState = [];
 
+export const addBook = (book) => ({
+  type: ADD_BOOK,
+  book,
+});
 
+export const removeBook = (bookID) => ({
+  type: REMOVE_BOOK,
+  bookID,
+});
