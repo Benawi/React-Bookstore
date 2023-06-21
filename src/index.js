d3935e43a0a7972cd6682795fe2c6b1165c2e4e8
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-06-21 09:16:32 +0300

    Wrap your <App> with the <Provider> component from react-redux

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import store from './redux/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
+    <Provider>
       <App />
     </Provider>
   </React.StrictMode>,
