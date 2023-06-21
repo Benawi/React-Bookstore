e21d82c6435548dccccfd2a644eb6167a343c9ca
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-06-21 09:21:30 +0300

    Display books,which received from the slice, in a reusable component

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,10 +1,49 @@
-import AddBook from './AddBook';
+import { FcFullTrash } from 'react-icons/fc';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/booksSlice';
+import './AddBook.css';
+import ChapterProgress from './ChapterProgress';
+import ChapterUpdateProgress from './ChapterUpdateProgress';
 
-const BookList = () => (
-  <div className="booksContainer">
-    <AddBook />
-  </div>
+const BooksList = () => {
+  const dispatch = useDispatch();
+  const books = useSelector((state) => state.books);
+  return (
+    <section className="section">
+      <div className="container-main">
+        {books.map((book) => (
+          <div className="content" key={book.item_id}>
+            <div className="aside">
+              <div className="aside-left" style={{ flex: 5 }}>
+                <div className="cat">{book.title}</div>
+                <div className="title">{book.author}</div>
+                <div className="author">{book.category}</div>
+                <button
+                  type="button"
+                  className="remove-btn"
+                  onClick={() => dispatch(removeBook(book.item_id))}
+                >
+                  <FcFullTrash />
+                  Remove
+                </button>
 
-);
+              </div>
+              <div className="aside-center" style={{ flex: 2 }}>
 
-export default BookList;
+                <ChapterProgress />
+
+              </div>
+              <div className="aside-right" style={{ flex: 3 }}>
+                <ChapterUpdateProgress />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <hr />
+    </section>
+
+  );
+};
+
+export default BooksList;
