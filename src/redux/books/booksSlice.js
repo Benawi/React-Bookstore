c80537572eb80d8441bd5316f8e820c503454b41
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-06-21 09:19:23 +0300

    Create an initialState variable for booksSlice

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,14 +1,43 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const generateId = () => `item${Math.random().toString(36).substr(2, 9)}`;
+
+const initialState = [
+  {
+    item_id: 'item1',
+    title: 'The Great Gatsby',
+    author: 'John Smith',
+    category: 'Fiction',
+  },
+  {
+    item_id: 'item2',
+    title: 'Anna Karenina',
+    author: 'Leo Tolstoy',
+    category: 'Fiction',
+  },
+  {
+    item_id: 'item3',
+    title: 'The Selfish Gene',
+    author: 'Richard Dawkins',
+    category: 'Nonfiction',
+  },
+];
+
 const booksSlice = createSlice({
   name: 'books',
-  initialState: [],
+  initialState,
   reducers: {
     addBook: (state, action) => {
-      state.push(action.payload);
+      const newBook = {
+        item_id: generateId(),
+        title: action.payload.title,
+        author: action.payload.author,
+        category: action.payload.category,
+      };
+      state.push(newBook);
     },
     removeBook: (state, action) => {
-      const bookIndex = state.findIndex((book) => book.id === action.payload);
+      const bookIndex = state.findIndex((book) => book.item_id === action.payload);
       if (bookIndex !== -1) {
         state.splice(bookIndex, 1);
       }
