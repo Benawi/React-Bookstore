6968aaaf83401846eebabd9d366ee664f829cb07
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-06-22 00:10:17 +0300

    Refactor add book components features to persist changes

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,91 +1,102 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
 import { addBook } from '../redux/books/booksSlice';
 import './AddBook.css';
 
-const BookForm = () => {
+const categories = [
+  'Select Category',
+  'Action',
+  'Dystopian',
+  'Fantasy',
+  'History',
+  'Horror',
+  'Kids',
+  'Learning',
+  'Sci-Fi',
+  'Spritual',
+  'Romance',
+  'Thriller',
+  'Realist',
+];
+function AddBook() {
   const dispatch = useDispatch();
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
-  const [category, setCategory] = useState('');
+  const [category, setcategory] = useState('Select category');
 
-  const handleTitle = (e) => {
-    setTitle(e.target.value);
+  const handleSubmit = async () => {
+    if (author !== '' && title !== '') {
+      dispatch(
+        addBook({
+          item_id: uuidv4(),
+          title,
+          author,
+          category,
+        }),
+      )
+        .unwrap()
+        .then(() => {
+          setTitle('');
+          setAuthor('');
+        });
+    }
   };
-
-  const handleAuthor = (e) => {
-    setAuthor(e.target.value);
-  };
-
-  const handleCategory = (e) => {
-    setCategory(e.target.value);
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    dispatch(addBook({ title, author, category }));
-    setTitle('');
-    setAuthor('');
-    setCategory('');
-  };
-
-  const categories = [
-    { value: '', label: 'Category', disabled: true },
-    { value: 'Action', label: 'Action' },
-    { value: 'History', label: 'History' },
-    { value: 'Horror', label: 'Horror' },
-    { value: 'Kids', label: 'Kids' },
-    { value: 'Learning', label: 'Learning' },
-    { value: 'Sci-Fi', label: 'Sci-Fi' },
-  ];
 
   return (
-    <div className="container">
-      <h2 className="addhead">ADD NEW BOOK</h2>
-      <form id="addBook" onSubmit={handleSubmit}>
-        <div className="formcontrol">
-          <input
-            type="text"
-            className="input-text"
-            placeholder="Add Title"
-            style={{ flex: 4 }}
-            name="title"
-            required
-            value={title}
-            onChange={handleTitle}
-          />
-          <input
-            type="text"
-            className="input-text"
-            placeholder="Add Author"
-            style={{ flex: 3 }}
-            required
-            value={author}
-            onChange={handleAuthor}
-          />
-          <select
-            name="category"
-            className="input-select"
-            required
-            value={category}
-            style={{ flex: 2 }}
-            onChange={handleCategory}
-          >
-            {categories.map((category) => (
-              <option
-                key={category.value}
-                value={category.value}
-                disabled={category.disabled}
+    <>
+      <hr />
+      <div className="container">
+        <h2 className="addhead">ADD NEW BOOK</h2>
+        <div>
+          <form id="addBook">
+            <div className="formcontrol">
+              <input
+                type="text"
+                className="input-text"
+                placeholder="Book Title"
+                required
+                style={{ flex: 4 }}
+                name="title"
+                value={title}
+                onChange={(e) => {
+                  setTitle(e.target.value);
+                }}
+              />
+              <input
+                type="text"
+                className="input-text"
+                placeholder="Author Name"
+                style={{ flex: 3 }}
+                required
+                name="author"
+                value={author}
+                onChange={(e) => {
+                  setAuthor(e.target.value);
+                }}
+              />
+              <select
+                className="input-select"
+                value={category}
+                required
+                style={{ flex: 2 }}
+                onChange={(e) => setcategory(e.target.value)}
               >
-                {category.label}
-              </option>
-            ))}
-          </select>
-          <button style={{ flex: 1 }} type="submit">ADD BOOK</button>
+                {
+          categories.map((genre) => (
+            <option key={uuidv4()} value={genre}>{genre}</option>
+          ))
+        }
+              </select>
+              <button style={{ flex: 1 }} type="button" onClick={handleSubmit}>
+                Add
+              </button>
+            </div>
+          </form>
         </div>
-      </form>
-    </div>
+      </div>
+    </>
   );
-};
+}
 
-export default BookForm;
+export default AddBook;
