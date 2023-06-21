92532408e4db5599b5b5babbdcae41e1576a5d89
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-06-21 09:27:18 +0300

    Add form components including button component

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,76 +1,84 @@
-import { v4 as uuidv4 } from 'uuid';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { addBook } from '../redux/books/booksSlice';
-import '../index.css';
+import './AddBook.css';
 
-const bookGenres = [
-  'Horror',
-  'Fiction',
-  'Thriller',
-  'Dark',
-  'Historical',
-  'Romance',
-  'Science Fiction',
-  'Fantasy',
-  'Dystopian',
-  'Realist',
-];
-
-const AddBook = () => {
+const BookForm = () => {
   const dispatch = useDispatch();
-  const books = useSelector((state) => state.books);
-  let id;
-  let max = Number.MIN_VALUE;
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [category, setCategory] = useState('');
 
-  if (books.length === 0) {
-    id = 1;
-  } else {
-    books.forEach((obj) => {
-      if (obj.id > max) {
-        max = obj.id;
-      }
-    });
-
-    id = max + 1;
-  }
-
-  const submitBookToStore = (element) => {
-    const title = element.children[0].value;
-    const categories = element.children[1].value;
-
-    const newBook = {
-      id,
-      title,
-      categories,
-    };
-
-    // dispatch an action and pass it the newBook object (your action's payload)
-    dispatch(addBook(newBook));
+  const handleTitle = (e) => {
+    setTitle(e.target.value);
   };
 
+  const handleAuthor = (e) => {
+    setAuthor(e.target.value);
+  };
+
+  const handleCategory = (e) => {
+    setCategory(e.target.value);
+  };
+
+
+  const categories = [
+    { value: '', label: 'Category', disabled: true },
+    { value: 'Action', label: 'Action' },
+    { value: 'History', label: 'History' },
+    { value: 'Horror', label: 'Horror' },
+    { value: 'Kids', label: 'Kids' },
+    { value: 'Learning', label: 'Learning' },
+    { value: 'Sci-Fi', label: 'Sci-Fi' },
+  ];
+
   return (
-    <div className="addBookContainer">
-      <h2 className="fontStyle10 fontColor1 mb1">Add New Book</h2>
-      <form
-        className="formContainer"
-        onSubmit={(e) => {
-          e.preventDefault();
-          submitBookToStore(e.target);
-          e.target.children[0].value = '';
-        }}
-      >
-        <input className="addBookTitle fontStyle9 fontColor4" type="text" placeholder="Book title" />
-        <select className="addBookGenre fontStyle9 fontColor4">
-          {
-          bookGenres.map((genre) => (
-            <option key={uuidv4()} value={genre}>{genre}</option>
-          ))
-        }
-        </select>
-        <button type="submit" className="addBookBtn appDefaultButton fontStyle4 fontColor5">Add Book</button>
+    <div className="container">
+      <h2 className="addhead">ADD NEW BOOK</h2>
+      <form id="addBook" >
+        <div className="formcontrol">
+          <input
+            type="text"
+            className="input-text"
+            placeholder="Add Title"
+            style={{ flex: 4 }}
+            name="title"
+            required
+            value={title}
+            onChange={handleTitle}
+          />
+          <input
+            type="text"
+            className="input-text"
+            placeholder="Add Author"
+            style={{ flex: 3 }}
+            required
+            value={author}
+            onChange={handleAuthor}
+          />
+          <select
+            name="category"
+            className="input-select"
+            required
+            value={category}
+            style={{ flex: 2 }}
+            onChange={handleCategory}
+          >
+            {categories.map((category) => (
+              <option
+                key={category.value}
+                value={category.value}
+                disabled={category.disabled}
+              >
+                {category.label}
+              </option>
+            ))}
+          </select>
+          <button style={{ flex: 1 }} type="submit">ADD BOOK</button>
+        </div>
       </form>
     </div>
   );
 };
 
-export default AddBook;
+export default BookForm;
