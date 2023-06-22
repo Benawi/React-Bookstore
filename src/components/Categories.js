15f153a787f7e1fd1aea449cfe60c8a1fdc8698b
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-06-22 11:50:21 +0300

    Add styles for categories page components

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,9 +1,11 @@
+import { FcSettings } from 'react-icons/fc';
 import { useSelector } from 'react-redux';
 
 const Categories = () => {
   const categories = useSelector((state) => state.categories);
   return (
     <div className="categoriesPageContainer">
+      <FcSettings />
       <h1>{categories}</h1>
     </div>
   );
