a893f71f9e1a09e09e8276adbe1f09fe9db0346b
Benawi Alemayehu <habtamu.alemayehu@wku.edu.et>
2023-06-22 12:01:27 +0300

    Add styles for updated progress components

--- src/components/ChapterUpdateProgress.js
+++ src/components/ChapterUpdateProgress.js
@@ -1,13 +1,27 @@
-const ChapterUpdateProgress = () => (
-  <div className="ChapterUPContainer">
-    <div>
-      <p className="fontStyle4 fontColor1 mb05">current Chapter</p>
-      <p className="fontStyle3 fontColor1 mb1">Chapter 4</p>
+const ChapterUpdateProgress = () => {
+  const chapters = [];
+  for (let i = 1; i < 31; i += 1) {
+    const chapter = ` ${i}`;
+
+    chapters.push(chapter);
+  }
+
+  const randomChapter = Math.floor(Math.random() * (chapters.length - 1));
+
+  return (
+    <div className="ChapterUPContainer">
+      <div>
+        <p className="fontStyle4 fontColor1 mb05">CURRENT CHAPTER</p>
+        <p className="fontStyle4 fontColor1 mb1">
+          Chapter
+          {chapters[randomChapter]}
+        </p>
+      </div>
+      <div>
+        <button className="appDefaultButton  fontColor5" type="button">UPDATE PROGRESS</button>
+      </div>
     </div>
-    <div>
-      <button className="appDefaultButton fontStyle4 fontColor5" type="button">Update Progress</button>
-    </div>
-  </div>
-);
+  );
+};
 
 export default ChapterUpdateProgress;
